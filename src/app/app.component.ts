6cf9728141e22e4a5cc14dce123bf5288aef115f
N <neil.pete@live.com>
2020-07-31 00:02:04 +0800

    Make GlobeGL container more responsive on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef, OnInit } from '@angular/core';
 import Globe from 'globe.gl';
 import * as moment from 'moment-timezone';
 
@@ -11,19 +11,24 @@ import * as moment from 'moment-timezone';
 
 
 export class AppComponent implements OnInit {
-  headerHeight = 64;
-  footerHeight = 55;
-  
   timeNow: any;
   private globeGl: any;
   private globeContainerHeight: number;
   private globeContainerWidth: number;
 
-  ngOnInit() {
-    this.globeContainerHeight = window.innerHeight;
+  @ViewChild('header', { read: ElementRef }) header: ElementRef;
+  @ViewChild('footer', { read: ElementRef }) footer: ElementRef;
+
   constructor() {
     this.initClock();
   }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.initGlobe();
+  }
+
   private initClock() {
     const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
     this.timeNow = moment().tz(tz).format("hh:mmA z");
@@ -31,11 +36,17 @@ export class AppComponent implements OnInit {
       this.timeNow = moment().tz(tz).format("hh:mmA z");
     }, 60000)
   }
+
+  private initGlobe() {
+    const headerHeight = this.header.nativeElement.offsetHeight;
+    const footerHeight = this.footer.nativeElement.offsetHeight;
+    const globeGlContainerMargin = 4;
+    this.globeContainerHeight = window.innerHeight - headerHeight - footerHeight - globeGlContainerMargin;
     this.globeGl = Globe()
-    .height(this.globeContainerHeight - this.headerHeight - this.footerHeight)
+    .height(this.globeContainerHeight)
     .globeImageUrl('/assets/img/earth-night.jpg')
     .backgroundImageUrl('/assets/img/starscape.png')
-    .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+    .bumpImageUrl('/assets/img/earth-topology.png')
     (document.getElementById('globe'));
   }
 }
