24960b034d266bfe2aa32aad66b5df400c0ca11d
N <neil.pete@live.com>
2020-07-31 00:00:08 +0800

    Display time of user on Main View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import Globe from 'globe.gl';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,23 @@ export class AppComponent implements OnInit {
   headerHeight = 64;
   footerHeight = 55;
   
+  timeNow: any;
   private globeGl: any;
   private globeContainerHeight: number;
   private globeContainerWidth: number;
 
   ngOnInit() {
     this.globeContainerHeight = window.innerHeight;
+  constructor() {
+    this.initClock();
+  }
+  private initClock() {
+    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    this.timeNow = moment().tz(tz).format("hh:mmA z");
+    setInterval(() => {
+      this.timeNow = moment().tz(tz).format("hh:mmA z");
+    }, 60000)
+  }
     this.globeGl = Globe()
     .height(this.globeContainerHeight - this.headerHeight - this.footerHeight)
     .globeImageUrl('/assets/img/earth-night.jpg')
